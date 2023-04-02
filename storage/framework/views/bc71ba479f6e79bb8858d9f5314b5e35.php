ee858682612db7759691f3d2d377b9c632da4b20
Rasyid Andriansyah <100756215+Perfectcopy86@users.noreply.github.com>
2023-04-02 12:30:38 +0700

    Add files via upload

---
+++ storage/framework/views/bc71ba479f6e79bb8858d9f5314b5e35.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>ini adalah halaman contact</h1>
+    <h2><?php echo e($name); ?> </h2>
+    <h2><?php echo e($Phone); ?> </h2>
+</body>
+
+</html><?php /**PATH C:\xampp\htdocs\belajar-laravel\resources\views/contact.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f88fa6edf19b10260c06ca3bf9da3482.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>ini adalah product dengan id <?php echo e($id); ?></h1>
+</body>
+
+</html><?php /**PATH C:\xampp\htdocs\belajar-laravel\resources\views/product/detail.blade.php ENDPATH**/ ?>
